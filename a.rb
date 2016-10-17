49f7ac5f5537bfeeb94a2f5f0d9c73ecab693d16
Artoria <pochioly2008@gmail.com>
2016-10-17 17:11:06 +0800

    modified:   a.rb 	modified:   doremi.rb

--- a.rb
+++ a.rb
@@ -1,21 +1,35 @@
 require './doremi.rb'
-
+require 'sinatra'
 Doremi.new(<<-'EOF').run
- 
   <seq xmlns:r="react-like" xmlns:d="doremi">
+   
+   <![CDATA[
     def html(node)
       node.domain.parent.args.push(node.children.map{|x| x.to_s}.join)
     end
-    <r:root>
-      a = gets.chomp
-      if a == "Y" || a == "y"
-        <puts>"You choose yes"</puts>
-      else
-        puts <d:html>
-          <H1>Not found</H1>
-        </d:html>
-      end
-    </r:root>
+
+    register_namespace "sinatra" do |node|
+       path = node.attributes["url"]
+       method = node.name
+       node.children.each{|x| node.domain.runNode(x, node.binding) }
+       send(method, path, &node.block)
+    end
+   ]]>
+
+    
+    <seq xmlns:s="sinatra">
+      <s:get url="/">
+       <d:addBlock>
+         <seq xmlns:d="doremi" xmlns:s="sinatra">
+           <d:html>
+             <H1>Hello world</H1>
+           </d:html>
+         </seq>
+        </d:addBlock>
+      </s:get>
+    </seq>
+
+    
   </seq>
 EOF
 
--- doremi.rb
+++ doremi.rb
@@ -6,6 +6,7 @@ class Doremi
     @doc     = REXML::Document.new(@xml)
     @binding = binding
     @ns      = {}
+    @context  = []
     registers
   end
 
@@ -39,7 +40,7 @@ class Doremi
    if @ns.include?(a)
      @ns[a]
    else
-     raise "can't find namespace #{a}"
+     raise "can't find namespace [" + a.to_s + "]"
    end
   end
 
@@ -66,10 +67,18 @@ class Doremi
     @doc.children[1].result
   ensure
     Domain.pop
+  
   end  
 
-  def runNode(node, bd)
+  def runNode(node, bd, clear = false)
+    if clear
+      Domain.push self
+      @stack = []
+    end
     node.doremi self, bd
+    node.result
+  ensure
+    Domain.pop if clear    
   end
 end
 
@@ -83,30 +92,33 @@ module REXML
       self.args  = []
       self.block = nil
       self.binding = binding
-      if self.namespace != ""
+      eval("__self = nil; lambda{|node| __self = node}", binding).call(self)
+      if self.namespace != "" && self.namespace != nil        
         return domain.ns(self.namespace).call(self)
       end
       children.each{|x|
         domain.runNode x, self.binding
       }
-      a = self.attributes.map{|k, v| [k, v]}.to_h
-      if a.empty? 
+      a = self.attributes.map{|k, v| [k, v]}.select{|k, v| !(k =~ /^xmlns:/)}.to_h
+      if a.empty?
         self.result = eval("self", self.binding).send(name, *args, &block)
       else
-
         self.result = eval("self", self.binding).send(name, *args, a, &block)
       end
       parent.args.push self.result if parent
+    ensure
       domain.pop
     end
   end
 
   class Text
-    attr_accessor :binding
+    attr_accessor :binding, :result
     def doremi(domain, binding)
       self.binding = binding
-      domain.top.args.push(eval(to_s, binding)) if to_s!=""
+      domain.top.args.push(self.result = eval(to_s, binding)) if to_s.strip!="" && to_s != nil && domain.top
     end
+
+    
   end
 end
 
@@ -115,10 +127,16 @@ def register_namespace(a, b = nil, &c)
 end
 
 
-def seq(*, last) 
-  last 
+def seq(*a)
+  a[-1]
 end
 
 def id(a)
   a
+end
+
+def addBlock(node)
+   node.domain.parent.block = eval "lambda{
+      Doremi.new('').runNode(__self.children[1], __self.binding, true)
+   }", node.binding
 end
