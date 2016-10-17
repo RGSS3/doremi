0013204b452bbcf9613c546ec253d8902cbab7be
RGSS3 <924172963@qq.com>
2016-10-17 11:42:50 +0800

    Update a.rb

--- a.rb
+++ a.rb
@@ -1,9 +1,46 @@
 require 'rexml/document'
 class Doremi
+  Domain = []
   def initialize(xml, binding = TOPLEVEL_BINDING)
     @xml     = xml
     @doc     = REXML::Document.new(@xml)
     @binding = binding
+    @ns      = {}
+    registers
+  end
+
+  def registers
+    register "doremi" do |node|
+      eval("self", node.binding).send(node.name, node, node.name, &node.block)
+    end
+    register_namespace 'react-like' do |node|
+      #node is a root
+      a = node.children
+      b = a.map.with_index{|x, i|      
+        case x 
+        when REXML::Text
+          x
+        when REXML::Element
+          " ([__node.domain.runNode(__node.children[" + i.to_s + "], __node.binding), __node.args.pop][1]) "
+        when nil
+          ""
+        end
+      }.join("")
+      eval("__node = nil; lambda{|node| __node = node}", node.binding).call(node)
+      eval b, node.binding
+    end
+  end
+
+  def register(a, b = nil, &c)
+    @ns[a] = b || c
+  end
+
+  def ns(a)
+   if @ns.include?(a)
+     @ns[a]
+   else
+     raise "can't find namespace #{a}"
+   end
   end
 
   def push(obj)
@@ -23,9 +60,12 @@ class Doremi
   end
 
   def run
+    Domain.push self
     @stack = []
     runNode @doc.children[1], @binding
     @doc.children[1].result
+  ensure
+    Domain.pop
   end  
 
   def runNode(node, bd)
@@ -43,26 +83,21 @@ module REXML
       self.args  = []
       self.block = nil
       self.binding = binding
-
-      if self.namespace == "doremi"
-        return eval("self", self.binding).send(name, self, name, &block)
+      if self.namespace != ""
+        return domain.ns(self.namespace).call(self)
       end
-      case name
-      when /^[a-z]/
-        children.each{|x|
-          domain.runNode x, self.binding
-        }
-        a = self.attributes.map{|k, v| [k, v]}.to_h
-        if a.empty? 
-          self.result = eval("self", self.binding).send(name, *args, &block)
-        else
+      children.each{|x|
+        domain.runNode x, self.binding
+      }
+      a = self.attributes.map{|k, v| [k, v]}.to_h
+      if a.empty? 
+        self.result = eval("self", self.binding).send(name, *args, &block)
+      else
 
-          self.result = eval("self", self.binding).send(name, *args, a, &block)
-        end
-        parent.args.push self.result if parent
-        domain.pop
+        self.result = eval("self", self.binding).send(name, *args, a, &block)
       end
-      
+      parent.args.push self.result if parent
+      domain.pop
     end
   end
 
@@ -75,6 +110,9 @@ module REXML
   end
 end
 
+def register_namespace(a, b = nil, &c)
+  Doremi::Domain.last.register a, b, &c
+end
 
 def seq(*, last) 
   last 
@@ -107,12 +145,12 @@ def twice
   yield
 end
 
-r = Doremi.new(%{
- <seq xmlns:d="doremi">
-   a = 3
-   b = 5
-   <p> a + b </p>
+r = Doremi.new(<<-'EOF')
+ <seq xmlns:d="doremi" xmlns:r="react-like" xmlns:q="quark"> 
+    <q:on key="ctrl-c">
+      <q:primitive command="selection.copy"></q:primitive>
+    </q:on>
  </seq>
-})
+EOF
 
 r.run
