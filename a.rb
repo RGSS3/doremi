c862a42aba0856b62fe5b6e79e87e0cc88c69f4f
Artoria <pochioly2008@gmail.com>
2016-10-18 10:02:20 +0800

    add example of execjs

--- a.rb
+++ a.rb
@@ -1,9 +1,22 @@
 require './doremi.rb'
+require 'execjs'
 Doremi.new(<<-'EOF').run
-  <seq xmlns:r="react-like" xmlns:d="doremi">
-     <r:root>
-       a = <Integer>3</Integer> + <Integer>5</Integer>
-       p a
+  <seq xmlns:r="react-like" xmlns:d="doremi" xmlns:js="execjs">
+     register_namespace 'execjs' do |o|
+        name = o.name
+        text = o.children.map{|x| x.to_s}.join
+        o.sink.push ExecJS.send(name, text)
+     end
+     <r:root>     
+       p <js:eval>
+         (function(G){
+           var s = 0;
+           for(var i = 1; i != 101; ++i){
+              s += i;
+           }
+           return s;
+          })(this)
+      </js:eval>
      </r:root>
   </seq>
 EOF
